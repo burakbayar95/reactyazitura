3ac99854d4af0d400fc1de4683d8e48751224c7e
Burak BAYAR <95burakbayar@gmail.com>
2021-05-14 13:23:13 +0300

    Updates

---
+++ precache-manifest.67707dfd35a3d6ead5313ea0d714679d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f5cf4c5c48a8dc29c38de1eb61627a",
+    "url": "/reactyazitura/index.html"
+  },
+  {
+    "revision": "98ad1919adfb5da457e9",
+    "url": "/reactyazitura/static/css/main.fea49381.chunk.css"
+  },
+  {
+    "revision": "6f46b9c97abacbd0f2be",
+    "url": "/reactyazitura/static/js/2.2ecc874f.chunk.js"
+  },
+  {
+    "revision": "98ad1919adfb5da457e9",
+    "url": "/reactyazitura/static/js/main.6e7f1402.chunk.js"
+  },
+  {
+    "revision": "595708b6de997d9c7283",
+    "url": "/reactyazitura/static/js/runtime~main.c03038fd.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/reactyazitura/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/reactyazitura/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
